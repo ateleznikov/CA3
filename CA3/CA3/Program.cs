b4ee5b80e9b67891d6010949fb371926f6a2ac22
ateleznikov <s00240505@atu.ie>
2023-04-04 17:06:43 +0100

    Upd

--- CA3/CA3/Program.cs
+++ CA3/CA3/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace CA3
+﻿/* Name: CA3 
+* Author: Anton Teleznikov
+* Date Created : 03/04/23
+* Purpose: 
+* Modified By:     
+*/
+using static System.Console;
+namespace CA3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            
+
+
+           
+
+
+
+
+        }
+        static int Menu()
+        {
+            string inputString;
+            int choice = 0;
+            bool choiceChecker = false;
+
+            WriteLine("Main Menu\n");
+            WriteLine("1. Ship Reports\n2. Occupation Report\n3. Age Report\n4. Exit"); 
+
+            while (choiceChecker == false)
+            {
+                Write("Enter Choice: ");
+                inputString = ReadLine();
+
+                if (int.TryParse(inputString, out int number))
+                {
+                    choice = number;
+                    if (choice >= 1 && choice <= 4)
+                    {
+                        choiceChecker = true;
+                    }
+                    else
+                    {
+                        WriteLine("You entered invalid number!");
+                        choiceChecker = false;
+                    }
+                }
+                else
+                {
+                    WriteLine("Enter a number!");
+                    choiceChecker = false;
+                }
+            }
+            return choice;
         }
     }
 }
---
+++ CA3/CA3/passenger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CA3
+{
+    internal class Passenger
+    {
+        // Atributes
+        private string _name;
+        private string _surname;
+        private DateOnly _arrivalDate;
+        private string _sex;
+        private int _age;
+        private string _occupation;
+        private string _country;
+        private string _destination;
+        private string _embarkationCode;
+        private string _shipNumber;
+
+        public static string path;
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Surname { get { return _surname; } set { _surname = value; } }
+        public DateOnly ArrivalDate { get { return _arrivalDate;  } set { _arrivalDate = value; } }
+        public string Sex { get { return _sex; } set { _sex = value; } }
+        public int Age { get { return _age; } set { _age = value; } }
+        public string Occupation { get { return _occupation; } set { _occupation = value; } }
+        public string Country { get { return _country; } set { _country = value; } }
+        public string Destination { get { return _destination; } set { _destination = value; } }
+        public string EmbarkationCode { get { return _embarkationCode; } set { _embarkationCode = value; } }
+        public string ShipNumber { get { return _shipNumber; } set { _shipNumber = value; } }
+
+        public Passenger() 
+        {
+            string path = @"../../../faminefile.csv";
+        }
+
+        static void AddInfo()
+        {
+            string path = @"../../../faminefile.csv";
+            int lineCount = File.ReadLines(path).Count();
+            string[,] passengerData = new string[lineCount, 10];
+            string? line;
+            int i = 0;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var values = line.Split(',');
+                    if (values.Length != 10)
+                    {
+                        throw new FormatException("Length is not equal to 10");
+                    }
+                    for (int j = 0; j < passengerData.GetLength(1); j++)
+                    {
+                        passengerData[i, j] = values[j];
+                    }
+                    i++;
+                }
+
+            }
+        }
+    }
+}
