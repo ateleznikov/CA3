38cd77d9e6587f886214044ff3cb980d6e88f4f7
ateleznikov <s00240505@atu.ie>
2023-04-04 20:25:24 +0100

    upd

--- CA3/CA3/Program.cs
+++ CA3/CA3/Program.cs
@@ -11,16 +11,31 @@ namespace CA3
     {
         static void Main(string[] args)
         {
+            int infantCounter = 0;
 
+            Passenger passenger = new Passenger();
+            string[,] passengerData = AddInfo();
 
-            
+            Passenger[] passengers = new Passenger[passengerData.GetLength(0)];
+            for (int i = 0; i < passengerData.GetLength(0); i++)
+            {
+                Passenger psngr = new Passenger();
+                {
+                    psngr.Name = passengerData[i, 0];
+                    psngr.Surname = passengerData[i, 1];
+                    psngr.Age = passengerData[i, 2];
+                    psngr.Sex = passengerData[i, 3];
+                    psngr.Occupation = passengerData[i, 4];
+                    psngr.Country = passengerData[i, 5];
+                    psngr.Destination = passengerData[i, 6];
+                    psngr.EmbarkationCode = passengerData[i, 7];
+                    psngr.ShipNumber = passengerData[i, 8];
+                    psngr.ArrivalDate = passengerData[i, 9];
+                }
+                passengers[i] = psngr;
+            }
 
-
-           
-
-
-
-
+            Passenger.Hujnya(passengers);
         }
         static int Menu()
         {
@@ -56,6 +71,33 @@ namespace CA3
                 }
             }
             return choice;
+        }
+        static string[,] AddInfo()
+        {
+            string path = @"../../../faminefile.csv";
+            int lineCount = File.ReadLines(path).Count();
+            string[,] passengerData = new string[lineCount, 10];
+            string? line;
+            int i = 0;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var values = line.Split(',');
+                    if (values.Length != 10)
+                    {
+                        throw new FormatException("Length is not equal to 10");
+                    }
+                    for (int j = 0; j < passengerData.GetLength(1); j++)
+                    {
+                        passengerData[i, j] = values[j];
+                    }
+                    i++;
+                }
+
+            }
+            return passengerData;
         }
     }
 }
--- CA3/CA3/passenger.cs
+++ CA3/CA3/passenger.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
+using System.Text.RegularExpressions;
 
 namespace CA3
 {
@@ -12,57 +13,73 @@ namespace CA3
         // Atributes
         private string _name;
         private string _surname;
-        private DateOnly _arrivalDate;
+        private string _arrivalDate;
         private string _sex;
-        private int _age;
+        private string _age;
         private string _occupation;
         private string _country;
         private string _destination;
         private string _embarkationCode;
         private string _shipNumber;
 
-        public static string path;
+        public int infantCounter;
         public string Name { get { return _name; } set { _name = value; } }
         public string Surname { get { return _surname; } set { _surname = value; } }
-        public DateOnly ArrivalDate { get { return _arrivalDate;  } set { _arrivalDate = value; } }
+        public string ArrivalDate { get { return _arrivalDate;  } set { _arrivalDate = value; } }
         public string Sex { get { return _sex; } set { _sex = value; } }
-        public int Age { get { return _age; } set { _age = value; } }
+        public string Age
+        { 
+            get 
+            { 
+                return _age; 
+            } 
+            set 
+            {
+                if (value.Contains("Infant"))
+                {
+                    infantCounter++;
+                    _age = value;
+                }
+                else
+                {
+                    _age = Regex.Replace(value, "[^0-9]", "");
+                    if (_age == "")
+                    {
+                        _age = "unknown";
+                    }
+                    else if (_age[0] == '0')
+                    {
+                        _age = _age.Substring(1);
+                    }
+                }
+            } 
+        }
         public string Occupation { get { return _occupation; } set { _occupation = value; } }
         public string Country { get { return _country; } set { _country = value; } }
         public string Destination { get { return _destination; } set { _destination = value; } }
         public string EmbarkationCode { get { return _embarkationCode; } set { _embarkationCode = value; } }
         public string ShipNumber { get { return _shipNumber; } set { _shipNumber = value; } }
 
-        public Passenger() 
+        public Passenger()
         {
-            string path = @"../../../faminefile.csv";
-        }
 
-        static void AddInfo()
+        }
+        public Passenger(string name, string surname, string arrivalDate, string sex, string age, string occupation, string country, string destination, string embarkationCode, string shipNumber)
         {
-            string path = @"../../../faminefile.csv";
-            int lineCount = File.ReadLines(path).Count();
-            string[,] passengerData = new string[lineCount, 10];
-            string? line;
-            int i = 0;
-
-            using (StreamReader sr = new StreamReader(path))
-            {
-                while ((line = sr.ReadLine()) != null)
-                {
-                    var values = line.Split(',');
-                    if (values.Length != 10)
-                    {
-                        throw new FormatException("Length is not equal to 10");
-                    }
-                    for (int j = 0; j < passengerData.GetLength(1); j++)
-                    {
-                        passengerData[i, j] = values[j];
-                    }
-                    i++;
-                }
-
-            }
+            Name = name;
+            Surname = surname;
+            ArrivalDate = arrivalDate;
+            Sex = sex;
+            Age = age;
+            Occupation = occupation;
+            Country = country;
+            Destination = destination;
+            EmbarkationCode = embarkationCode;
+            ShipNumber = shipNumber;
         }
-    }
+        public static void Hujnya(Passenger[] passengers)
+        {
+            WriteLine(passengers[1].Age);
+        }
+}
 }
